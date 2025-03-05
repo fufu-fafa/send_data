7e11ca71d839833c546058a2870604b3ec72714f
n1993rs <osintstuf@gmail.com>
2025-03-05 17:28:54 +0700

    1

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def index():
 
 @app.route('/data')
 def data():
-    return {"message": "01001000 01100101 01101100 01101100 01101111 00101100 00100000 01010111 01101111 01110010 01101100 01100100 00100001"}
+    return {"message": "01001000 01100101 01101100 01101100 01101111 00101100 00100000 01010111 01101111 01110010 01101100 01100100 00100001 long long long longlon long long long long"}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=50000, debug=True)
